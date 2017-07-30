8e72c46d71bdcd81eeb58cd215282b3308aa9d62
Milo Knowles <mknowles@mit.edu>
2017-07-30 16:12:44 -0700

    Training routine now compiles...

---
+++ training.py
@@ -0,0 +1,90 @@
+from keras import backend as K
+from keras.models import Model
+from keras.layers import Input, Dense, Concatenate
+
+from model.bilinear_tensor_product import BilinearTensorProduct
+
+from dataset import loadInputTensor
+import numpy as np
+
+import tensorflow as tf
+
+def contrastive_max_margin_loss(y_true, y_pred):
+	"""
+	@param y_true : a size 2 vector containing [real_score, corrupt_score]
+	"""
+	loss = tf.maximum(0.0, 1 + tf.subtract(y_pred[:,1:], y_pred[:,0:])) # right - left
+	return loss
+
+
+def confusion(y_true, y_pred):
+	"""
+	Simply returns -1 if the corrupt tuple scores better than the real tuple.
+	else returns 0
+	"""
+	if y_pred[0] < y_pred[1]:
+		return -1
+	else:
+		return 0
+
+def loadData(num_batches):
+	"""
+	Creates a giant tensor of shape (6, n, d)
+	where n is the number of training examples
+	and d is the dimension of word embedding vectors
+	"""
+	file_batch_shape = loadInputTensor(1).shape
+	data = np.zeros((file_batch_shape[0], num_batches*file_batch_shape[1], file_batch_shape[2]))
+	for i in range(num_batches):
+		np.concatenate((data, loadInputTensor(i)), axis=1)
+	return data
+
+data = loadData(20)
+
+# DEFINE CONSTANTS
+n = data.shape[1]
+batch_size = 16
+num_epochs = 50
+d = 300
+k = 40
+l = 20
+
+subj_input = Input(shape=(d,), dtype='float32', name='subj_input')
+act_input = Input(shape=(d,), dtype='float32', name='act_input')
+pred_input = Input(shape=(d,), dtype='float32', name='pred_input')
+
+subj_corr_input = Input(shape=(d,), dtype='float32')
+act_corr_input = Input(shape=(d,), dtype='float32')
+pred_corr_input = Input(shape=(d,), dtype='float32')
+
+# embed object/action pairs into relations
+shared_btp_1 = BilinearTensorProduct(k, input_dim=d, name='shared_tensor1')
+shared_btp_2 = BilinearTensorProduct(k, input_dim=d, name='shared_tensor2')
+
+out1 = shared_btp_1([subj_input, act_input])
+out2 = shared_btp_2([pred_input, act_input])
+
+out3 = shared_btp_1([subj_corr_input, act_corr_input])
+out4 = shared_btp_2([pred_corr_input, act_corr_input])
+
+# embed relation/relation pairs into events
+shared_btp_3 = BilinearTensorProduct(l, input_dim=k, name='shared_tensor3')
+real_embedding = shared_btp_3([out1, out2])
+corr_embedding = shared_btp_3([out3, out4])
+
+# calculate embedding scores
+shared_linear = Dense(1, input_shape=(l,), name='linear_add_layer')
+real_score = shared_linear(real_embedding)
+corr_score = shared_linear(corr_embedding)
+
+# concatenate the 2 outputs into a single output
+output = Concatenate(axis=1, name='concat_output')([real_score, corr_score])
+
+model = Model(input=[subj_input, act_input, pred_input, subj_corr_input, act_corr_input, pred_corr_input],
+							outputs=[output])
+
+model.compile(optimizer='sgd',
+              loss=contrastive_max_margin_loss)
+
+model.fit([data[0], data[1], data[2], data[3], data[4], data[5]],
+					[np.ones((n, 2))], epochs=num_epochs, batch_size=batch_size)
